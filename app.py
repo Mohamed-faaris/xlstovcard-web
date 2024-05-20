2954aefb36da5820ea7a55b08b7fedceb8ca3ad9
Mohamed Faaris <mohammedfaaris2005@gmail.com>
2024-05-21 00:29:01 +0530

    nums

--- app.py
+++ app.py
@@ -2,47 +2,42 @@ from flask import Flask,render_template,request,session,redirect
 from contact.contact import *
 from flask_session import Session
 
-contacts = Contacts(" ")
 app = Flask(__name__)
-
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
 @app.route('/')
 def index():
-    return render_template('name.html',columns = contacts.columns)
+    session["contact"] = Contacts("")
+    return render_template('name.html',columns = session["contact"].columns)
 
 @app.route("/number",methods=['GET', 'POST'])
 def number():
-    print(request.form)
+    session["contact"].num_index_label =[ [request.form.get(f'num_label-{i}',""),int(request.form.get(f'num_index-{i}',-1))] for i in range(session.get("no_of_number",2))]  
+    print(session["contact"].num_index_label)
     if request.method == "POST":
-        return render_template('number.html',columns = contacts.columns,no_of_number = session.get("no_of_number",2))
-    return render_template('number.html',columns = contacts.columns,no_of_number = session.get("no_of_number",2))
+        return render_template('number.html',columns = session["contact"].columns,no_of_number = session.get("no_of_number",2),phone = session["contact"].num_index_label)
+    return render_template('number.html',columns = session["contact"].columns,no_of_number = session.get("no_of_number",2),phone = session["contact"].num_index_label)
 
 @app.route("/no_of_number",methods=['GET', 'POST'])
 def num_number():
     try:
         session["no_of_number"] = int(request.form["no_of_number"])
-        #no_of_number = int(request.args.get('no_of_number'))
     except:
         pass
     return redirect("/number")
 
 @app.route("/email",methods=['GET', 'POST'])
 def email():
-    phone_index_labels =[[0,0] for _ in session.get("no_of_email",2)] 
-    print(phone_index_labels)
     if request.method == "POST":
-        return render_template('email.html',columns = contacts.columns,no_of_email = session.get("no_of_email",2))
-    return render_template('email.html',columns = contacts.columns,no_of_email = session.get("no_of_email",2),phone = phone_index_labels)
+        return render_template('email.html',columns = session["contact"].columns,no_of_email = session.get("no_of_email",2))
+    return render_template('email.html',columns = session["contact"].columns,no_of_email = session.get("no_of_email",2))
 
 @app.route("/no_of_email",methods=['GET', 'POST'])
 def num_email():
     try:
         session["no_of_email"] = int(request.form["no_of_email"])
-        #no_of_email = int(request.args.get('no_of_email'))
     except:
         pass
     return redirect("/email")
@@ -50,13 +45,14 @@ def num_email():
 @app.route("/groups",methods=['GET', 'POST'])
 def groups():
     if request.method != "POST":
-        return render_template('groups.html',columns = contacts.columns)
+        return render_template('groups.html',columns = session["contact"].columns)
     
 @app.route("/addional",methods=['GET', 'POST'])
 def addional():
     if request.method != "POST":
-        return render_template('addional.html',columns = contacts.columns)
+        return render_template('addional.html',columns = session["contact"].columns)
     
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
